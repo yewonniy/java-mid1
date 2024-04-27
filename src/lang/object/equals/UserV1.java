c088641cb8af843d54665ad0d3a6be0c8fbfad95
yewonniy <ae6652@naver.com>
2024-04-27 22:06:42 +0900

    section1 : Object 클래스

---
+++ src/lang/object/equals/UserV1.java
@@ -0,0 +1,11 @@
+package lang.object.equals;
+
+import java.util.Objects;
+
+public class UserV1 {
+    private String id;
+
+    public UserV1(String id) {
+        this.id = id;
+    }
+}
---
+++ src/lang/object/toString/ObjectPrinter.java
@@ -0,0 +1,10 @@
+package lang.object.toString;
+
+public class ObjectPrinter {
+
+    public static void print(Object o) {
+        String string = "객체 정보 출력: " + o.toString();
+        System.out.println(string);
+    }
+
+}
